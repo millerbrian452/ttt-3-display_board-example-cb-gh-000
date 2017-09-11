e5de17c31f7a5394c7303cac75caaf149ee0b047
Brian Miller <jwino0004@mail.ct.edu>
2017-09-11 18:06:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,12 @@
 def display_board
-  cell = return "   ", "|", "   ", "|", "   "
-  line = return "-----------"
-  cell = return "   ", "|", "   ", "|", "   "
-  line = return "-----------"
-  cell = return "   ", "|", "   ", "|", "   "
+  print "   ", "|", "   ", "|", "   "
+  
+  print "-----------"
+
+  print "   ", "|", "   ", "|", "   "
+
+  print "-----------"
+
+  print "   ", "|", "   ", "|", "   "
+end
+
